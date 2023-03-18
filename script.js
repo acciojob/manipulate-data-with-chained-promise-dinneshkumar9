1873d85e9429a790e3e5ed0af4bd13f1a4d6cd21
Dinesh kumar <dinneshkumar9@gmail.com>
2023-03-18 10:44:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,21 @@
-//your JS code here. If required.
-const inputArray = [1, 2, 3, 4];
-
-// Function that returns a promise which resolves with an array of numbers after 3 seconds
-function generateNumberArray() {
-  return new Promise((resolve) => {
+function manipulateArray(arr) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(inputArray);
+      resolve(arr);
     }, 3000);
+  })
+  .then(arr => {
+    const filteredArr = arr.filter(num => num % 2 === 0);
+    document.getElementById("output").innerHTML = filteredArr.join(",");
+    return filteredArr;
+  })
+  .then(filteredArr => {
+    const multipliedArr = filteredArr.map(num => num * 2);
+    setTimeout(() => {
+      document.getElementById("output").innerHTML = multipliedArr.join(",");
+    }, 2000);
   });
 }
 
-// Chain the promise with two more promises
-generateNumberArray()
-  .then((numberArray) => {
-    // Filter out odd numbers
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const filteredArray = numberArray.filter((number) => number % 2 === 0);
-        resolve(filteredArray);
-      }, 1000);
-    });
-  })
-  .then((evenNumbersArray) => {
-    // Multiply all even numbers by 2
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const multipliedArray = evenNumbersArray.map((number) => number * 2);
-        resolve(multipliedArray);
-      }, 2000);
-    });
-  })
-  .then((finalArray) => {
-    // Update the text of an HTML element with an ID of "output"
-    const outputDiv = document.getElementById("output");
-    outputDiv.innerHTML = finalArray.join(", ");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+const inputArr = [1, 2, 3, 4];
+manipulateArray(inputArr);
